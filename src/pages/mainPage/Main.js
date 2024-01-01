cb87439418d84a0de796cf6853e4302accc9cc57
sehxxnee <shyan5253@naver.com>
2024-01-02 03:20:52 +0900

    Feat: Add moving melody icon

--- src/pages/mainPage/Main.js
+++ src/pages/mainPage/Main.js
@@ -22,21 +22,34 @@ const Main = () => {
     <div className="mainPage">
       <img
         className="mainPageBackGround" 
-        src={`${process.env.PUBLIC_URL}/img/mainBackGround.png`}
+        src={`${process.env.PUBLIC_URL}/img/mainBackGroundWithNoMelody.png`}
         alt="Main Background"
         style={{ height: `${verticalHeight}px` }}
       />
+      <img
+        className="melody8thNote" 
+        src={`${process.env.PUBLIC_URL}/img/icons8-음표-60.png`} 
+      />
+      <img
+        className="melody2ndNoteBig" 
+        src={`${process.env.PUBLIC_URL}/img/icons8-음표-48.png`} 
+      />
+      <img
+        className="melody2ndNoteSmall" 
+        src={`${process.env.PUBLIC_URL}/img/icons8-음표-24.png`} 
+      />
+      
 
       <div className="mainPageLogo">Vibro Maestro</div>
       <div className="mainPageSubtitle">
         Musical instrument learning tool<br />
         　 for the hearing impaired
       </div>
-      
+
       <div className="buttonGroup">
         <button className="playButton">Play</button>
-        <button className="playButton">Listen</button>
-        <button className="playButton">Gallery</button>
+        <button className="ListenButton">Listen</button>
+        <button className="GalleryButton">Gallery</button>
       </div>
     </div>
     </>
